3a946e7f59deabe43b1b6c3f5cb8ca8a90eaafc5
Patrick wong <pattwong156@gmail.com>
2019-03-06 09:40:03 -0800

    no message

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
 
-var app = express() 
+var app = express();
+var port = process.env.PORT || 3000;
 
 //make a new server 
 app.use(express.static(__dirname)); 
 
 //listen for requests on port 3000
-var server = app.listen(3000, () =>{
+var server = app.listen(port, () =>{
     console.log('server is listening on port',server.address().port); 
 });
