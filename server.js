ac9f447d1d47758e26e4ff4f79075aee83dcf350
Jack <corintijohnson@gmail.com>
2019-03-05 23:34:28 -0800

    no message

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 var express = require('express');
 
 var app = express() 
-var port = process.env.PORT || 3000;
+
 //make a new server 
 app.use(express.static(__dirname)); 
 
 //listen for requests on port 3000
-var server = app.listen(port, () =>{
+var server = app.listen(3000, () =>{
     console.log('server is listening on port',server.address().port); 
 });
