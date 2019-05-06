a949edbe54e882f0f53d44c2ec38e1fa964d8319
solale90 <solaleh.rm@gmail.com>
2019-05-05 18:34:59 -0700

    Use food.js in restaurant.html

--- app.js
+++ app.js
@@ -51,3 +51,19 @@ var packageChange = {
 //PROXY
 // 2 arguments (theVariableToObserve, function()onWhatToDo)
 var p = new Proxy(package, packageChange);
+
+// Utils
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+        }
+    }
+};
---
+++ food.js
@@ -0,0 +1,499 @@
+var food = {
+    //Burgers Items
+
+    burgers: [{
+            name: "Bacon Cheddar Burger",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "Fish Burger",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 4
+        },
+        {
+            name: "Cheese Burger",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                ]
+            }],
+            description: "blabh",
+            price: 15,
+            rate: 3
+        }
+    ],
+
+    //Sushi Items
+
+    sushi: [{
+            name: "First Sushi",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "Second Sushi",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "third Sushi",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        }
+    ],
+
+    //Steak Items
+
+    steak: [{
+            name: "First Steak",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "Second Steak",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "third Steak",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        }
+    ],
+
+    //Pizza Item
+
+    pizza: [{
+            name: "First Pizza",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "Second Pizza",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "third Pizza",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        }
+    ],
+
+    //Pasta Item
+
+    pasta: [{
+            name: "First Pasta",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "Second Pasta",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "third Pasta",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        }
+    ],
+
+    //Chicken Item
+
+    chicken: [{
+            name: "First Chicken",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "Second Chicken",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        },
+        {
+            name: "third Chicken",
+            restaurants: [{
+                name: "TripleOs",
+                locations: [{
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    },
+                    {
+                        "City": "Vancouver",
+                        "Postal Code": "V33CF3",
+                        "Tel": "7785747383"
+                    }
+                ]
+            }],
+            description: "blabh",
+            price: 10,
+            rate: 2
+        }
+    ]
+}
+
+var categories = [{
+    id: 'burgers',
+    name: 'Burgers'
+},{
+    id: 'steak',
+    name: 'Steaks'
+},{
+    id: 'pasta',
+    name: 'Pasta'
+},
+{
+    id: 'pizza',
+    name: 'Pizza'
+},
+{
+    id: 'sushi',
+    name: 'Sushi'
+},{
+    id: 'chicken',
+    name: 'Chicken'
+}];
